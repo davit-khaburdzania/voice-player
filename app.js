b84891ed6649774bf4b817928d360a74298016d0
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-03-28 22:49:02 +0400

    youtube api

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.errorHandler());
 
 app.get('/', routes.index);
+app.get('/youtube', routes.youtube);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
